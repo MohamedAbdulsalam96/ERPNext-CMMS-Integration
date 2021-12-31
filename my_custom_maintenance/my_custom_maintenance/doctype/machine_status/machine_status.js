bb22f6a88c69a70e49c152c50b8096c6abbd02a6
chiajunshen <chiajjss@email.com>
2021-12-31 08:45:01 +0000

    Minor improvement

--- my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.js
+++ my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.js
@@ -2,8 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Machine Status', {
-	// refresh: function(frm) {
-	// },
+	refresh: function(frm) {
+		if (frm.doc.asset && frm.doc.workstation) {
+			if (frm.doc.source == "ERPNext") {
+				frm.set_df_property('machine_status', 'read_only', 0);
+				frm.set_df_property('availability', 'read_only', 0);
+				frm.set_df_property('performance', 'read_only', 0);
+				frm.set_df_property('oee', 'read_only', 0);
+				frm.set_df_property('pass_rate', 'read_only', 0);
+			}
+			else if (frm.doc.source == "Node-RED") {
+				frm.set_df_property('machine_status', 'read_only', 1);
+				frm.set_df_property('availability', 'read_only', 1);
+				frm.set_df_property('performance', 'read_only', 1);
+				frm.set_df_property('oee', 'read_only', 1);
+				frm.set_df_property('pass_rate', 'read_only', 1);
+			}
+		}
+	},
 
 	source: function(frm) {
 		if (frm.doc.asset && frm.doc.workstation) {
