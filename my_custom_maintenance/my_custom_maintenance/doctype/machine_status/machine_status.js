c1a2a1b22ea5a66721c6844a6054c91bcfec4f75
chiajunshen <chiajjss@gmail.com>
2022-01-07 03:01:35 +0000

    Added an option to sync with Node-RED

--- my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.js
+++ my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.js
@@ -4,14 +4,14 @@
 frappe.ui.form.on('Machine Status', {
 	refresh: function(frm) {
 		if (frm.doc.asset && frm.doc.workstation) {
-			if (frm.doc.source == "ERPNext") {
+			if (frm.doc.sync_with_nr == 0) {
 				frm.set_df_property('machine_status', 'read_only', 0);
 				frm.set_df_property('availability', 'read_only', 0);
 				frm.set_df_property('performance', 'read_only', 0);
 				frm.set_df_property('oee', 'read_only', 0);
 				frm.set_df_property('pass_rate', 'read_only', 0);
 			}
-			else if (frm.doc.source == "Node-RED") {
+			else if (frm.doc.sync_with_nr == 1) {	// sync with node-red, make fields READ_ONLY since data is fetched from node-red
 				frm.set_df_property('machine_status', 'read_only', 1);
 				frm.set_df_property('availability', 'read_only', 1);
 				frm.set_df_property('performance', 'read_only', 1);
@@ -21,22 +21,36 @@ frappe.ui.form.on('Machine Status', {
 		}
 	},
 
-	source: function(frm) {
+	sync_with_nr: function(frm) {
 		if (frm.doc.asset && frm.doc.workstation) {
-			if (frm.doc.source == "ERPNext") {
+			if (frm.doc.sync_with_nr == 0) {
 				frm.set_df_property('machine_status', 'read_only', 0);
 				frm.set_df_property('availability', 'read_only', 0);
 				frm.set_df_property('performance', 'read_only', 0);
 				frm.set_df_property('oee', 'read_only', 0);
 				frm.set_df_property('pass_rate', 'read_only', 0);
 			}
-			else if (frm.doc.source == "Node-RED") {
-				frm.set_df_property('machine_status', 'read_only', 1);
-				frm.set_df_property('availability', 'read_only', 1);
-				frm.set_df_property('performance', 'read_only', 1);
-				frm.set_df_property('oee', 'read_only', 1);
-				frm.set_df_property('pass_rate', 'read_only', 1);
+			else if (frm.doc.sync_with_nr == 1) {	// sync with node-red, make fields READ_ONLY since data is fetched from node-red
+
+				// frm.call('check_if_API_exist',{ ms_id: frm.doc.name});
+				frm.call('check_if_API_exist').then(r => {	// check if there is a Server Script API named as "Mac Stat From NR To ERPNext"
+					if (r.message == "API not found") {
+						frappe.msgprint({
+							title: __('Warning: Fail to sync.'),
+							indicator: 'red',
+							message: __('Reason: Server Scipt API: "Mac Stat From NR To ERPNext" not found. Please name your API correctly.'),
+						});
+						frm.reload_doc();
+					}
+					else if (r.message == "API exists") {	// API exists
+						frm.set_df_property('machine_status', 'read_only', 1);
+						frm.set_df_property('availability', 'read_only', 1);
+						frm.set_df_property('performance', 'read_only', 1);
+						frm.set_df_property('oee', 'read_only', 1);
+						frm.set_df_property('pass_rate', 'read_only', 1);
+					}
+				});
 			}
 		}
-	}
+	},
 });
--- my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.py
+++ my_custom_maintenance/my_custom_maintenance/doctype/machine_status/machine_status.py
@@ -3,8 +3,15 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MachineStatus(Document):
-	pass
+
+	# return True if API exist in database, otherwise return False.
+	@frappe.whitelist()
+	def check_if_API_exist(self):
+		if frappe.db.exists("Server Script", "Mac Stat From NR To ERPNext"):
+			return "API exists"
+		else:
+			return "API not found"
